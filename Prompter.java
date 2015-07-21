8a6a3aafa5515190c8161bccea82b7de4d3cac2a
Mom <mom@Michaels-MacBook-Pro-2.local>
2015-07-21 12:20:57 -0700

    add functions to hangman.java

---
+++ Prompter.java
@@ -0,0 +1,45 @@
+import java.io.Console;
+
+public class Prompter {
+  private Game mGame;
+
+  public Prompter(Game game) {
+    mGame = game;
+  }
+
+  public void play() {
+    while (mGame.getRemainingTries() > 0) {
+      displayProgress();
+      promptForGuess();
+    }
+  }
+
+  public boolean promptForGuess() {
+    Console console = System.console();
+    boolean isHit = false;
+    boolean isValidGuess = false;
+
+    while (!isValidGuess) {
+      String guessAsString = console.readLine("Enter a letter:  ");
+      try {
+        isHit = mGame.applyGuess(guessAsString);
+        isValidGuess = true;
+      }
+      catch (IllegalArgumentException iae) {
+        console.printf("%s.  Please try again.\n", iae.getMessage());
+
+      }
+    }
+
+    return isHit;
+  }
+
+  public void displayProgress() {
+    System.out.printf("You have %d tries left to solve:  %s\n",
+                      mGame.getRemainingTries(),
+                      mGame.getCurrentProgress());
+  }
+
+
+
+}
